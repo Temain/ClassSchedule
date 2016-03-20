51406639947ab4352315def7c01a91c351b8088b
Temain <asv.artm@gmail.com>
2016-03-20 06:36:40 +0300

    Заполнение ячейки сетки

---
+++ ClassSchedule.Web/Helpers/Helpers.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace ClassSchedule.Web.Helpers
+{    
+    public static class Helpers
+    {
+        public static MvcHtmlString TimeOfLesson(int lessonDay, int lessonNumber)
+        {
+            string[][] weekdaysTime = new[]
+            {
+                new[] {"08:00", "09:30"},
+                new[] {"09:45", "11:15"},
+                new[] {"11:30", "13:00"},
+                new[] {"13:50", "15:20"},
+                new[] {"15:35", "17:05"},
+                new[] {"17:20", "18:50"}
+            };
+
+            string[][] saturdayTime = new[]
+            {
+                new[] {"08:00", "09:30"},
+                new[] {"09:45", "11:15"},
+                new[] {"11:30", "13:00"},
+                new[] {"13:15", "14:45"},
+                new[] {"15:00", "16:30"},
+                new[] {"16:45", "18:15"}
+            };
+
+            string result;
+            if (lessonDay != 5) // Не суббота
+            {
+                result = weekdaysTime[lessonNumber][0] + "<br>" + weekdaysTime[lessonNumber][1];
+            }
+            else
+            {
+                result = saturdayTime[lessonNumber][0] + "<br>" + saturdayTime[lessonNumber][1];
+            }
+
+            return MvcHtmlString.Create(result);
+        }
+    }
+}
