023f7b38632ebad20582b4f37a220ce70ba9355b
Temain <asv.artm@gmail.com>
2016-04-15 15:59:33 +0300

    !@#$% git

---
+++ ClassSchedule.Web/Models/Teacher/TeacherLesson.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace ClassSchedule.Web.Models.Teacher
+{
+    public class TeacherLesson
+    {
+        public int DayNumber { get; set; }
+        public int ClassNumber { get; set; }
+
+        /// <summary>
+        /// Это окно в расписании преподавателя
+        /// </summary>
+        public bool IsDowntime { get; set; }
+
+        public IEnumerable<TeacherDiscipline> Disciplines { get; set; } 
+    }
+}
